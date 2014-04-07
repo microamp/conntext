951ec8812d23937d7fc9dbcd3b0a9d7c869b805d
han semaj <sangho.nah@gmail.com>
2014-04-07 23:04:50 +1200

    now supports: python 2.7, 3.3 and pypy

--- setup.py
+++ setup.py
@@ -38,7 +38,5 @@ setup(name="Conntext",
                    "Operating System :: OS Independent",
                    "Programming Language :: Python",
                    "Programming Language :: Python :: 2.7",
-                   "Programming Language :: Python :: 3.1",
-                   "Programming Language :: Python :: 3.2",
                    "Programming Language :: Python :: 3.3",
                    "Topic :: Software Development :: Libraries",))
