ddebd794d2b93f76f83c17ba2a87af13d564c521
han semaj <sangho.nah@gmail.com>
2014-04-14 19:59:14 +1200

    version 0.1.5: setup script revisited

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
-## Conntext - Context managers for secure and atomic database connectivity
-## Copyright (C) 2014  James Nah <sangho.nah@gmail.com>
-##
-## This library is free software: you can redistribute it and/or
-## modify it under the terms of the GNU Lesser General Public License
-## as published by the Free Software Foundation, either version 3 of
-## the License, or (at your option) any later version.
-##
-## This library is distributed in the hope that it will be useful, but
-## WITHOUT ANY WARRANTY; without even the implied warranty of
-## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-## Lesser General Public License for more details.
-##
-## You should have received a copy of the GNU Lesser General Public
-## License along with this program. If not, see <http://www.gnu.org/licenses/>.
+# Conntext - Context managers for secure and atomic database connectivity
+# Copyright (C) 2014  James Nah <sangho.nah@gmail.com>
+#
+# This library is free software: you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public License
+# as published by the Free Software Foundation, either version 3 of
+# the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 from setuptools import setup, find_packages
 
@@ -27,7 +27,7 @@ setup(name="Conntext",
       author_email="sangho.nah@gmail.com",
       url="https://github.com/microamp/conntext",
       packages=find_packages(),
-      package_data={"conntext": ["README.rst", "LICENSE", "LGPL"]},
+      package_data={"": ["README.rst", "LICENSE", "LGPL"]},
       include_package_data=True,
       install_requires=[],
       license=open("LICENSE").read(),
